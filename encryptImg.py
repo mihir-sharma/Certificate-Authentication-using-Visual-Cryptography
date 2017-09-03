c0499da93c3611c26ee5cba4ba8944393c252de7
mihir-sharma <mihir.sharma@st.niituniversity.in>
2017-09-04 01:16:52 +0530

    Update encryptImg.py

--- encryptImg.py
+++ encryptImg.py
@@ -21,27 +21,28 @@ def encryptImg(x, y, z, w):
     keyServer.close()
 
     img = cv2.imread(z, -1)
+    width, height = img.shape[:2]
     arr = []
-    for i in range(0, 585):
-        for j in range(0, 585):
-            b = int(img[i, j, 0] ^ x[585 * i + j][0])
-            g = int(img[i, j, 1] ^ x[585 * i + j][1])
-            r = int(img[i, j, 2] ^ x[585 * i + j][2])
+    for i in range(0, width):
+        for j in range(0, height):
+            b = int(img[i, j, 0] ^ x[height * i + j][0])
+            g = int(img[i, j, 1] ^ x[height * i + j][1])
+            r = int(img[i, j, 2] ^ x[height * i + j][2])
             arr.append([b, g, r])
 
     shareCert.write(json.dumps(np.asarray(arr).tolist()))
     shareCert.close()
 
     imgshareClient = open(y, "r")
-    img = np.zeros((585,585,3), np.uint8)
+    img = np.zeros((width,height,3), np.uint8)
     img[:] = (255,255,255)
     y = np.array(json.load(imgshareClient))
 
-    for i in range(0,585):
-        for j in range(0,585):
-            bl = y[i*585+j][0]
-            gr = y[i*585+j][1]
-            rd = y[i*585+j][2]
+    for i in range(0, width):
+        for j in range(0, height):
+            bl = y[i*height+j][0]
+            gr = y[i*height+j][1]
+            rd = y[i*height+j][2]
             img.itemset((i, j, 0), bl)
             img.itemset((i, j, 1), gr)
             img.itemset((i, j, 2), rd)
